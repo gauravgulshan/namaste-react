21788f9095921a5412c8645999244371b77dfea9
Gaurav Gulshan <gaurav@192.168.1.5>
2025-01-08 19:08:30 +0530

    Episode 03

--- App.js
+++ App.js
@@ -3,18 +3,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // "react-dom" throws error
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "I'm an h1 tag in child1"),
-        React.createElement("h2", {}, "I'm an h2 tag in child1"),
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I'm an h1 tag in child2"),
-        React.createElement("h2", {}, "I'm an h2 tag in child2"),
-    ]),
-]);
+// React.createElement => ReactElement - JS Object => HTMLElement(render)
+// Using Core React
+const heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Namaste React!"
+);
 
-console.log(parent); //object
+// JSX - HTML like or XML like syntax
+// JSX => (babel) => React.createElement => ReactElement - JS Object => HTMLElement(render)
+const jsxHeading = <h1 id = "heading">Namaste React using JSX!</h1>
+
+// Output of both should be same
+console.log(heading); //object
+console.log(jsxHeading); 
+
+const Title = () => {
+    return (
+    <div>
+        <h2>Nested Title Component!</h2>
+    </div>
+    );
+};
+
+const num = 10;
+
+// React Functional Component 
+const HeadingComponent = () => {
+    return (
+    <div id = "container">
+        <h1>Namste React Functional Component!</h1>
+        {/* <Title /> */}
+        {/* <Title></Title> */}
+        {Title()}
+        <h3>Printing num - {num} though js code using curly braces!</h3>
+        <h3>Since heading is a React element, which is just a JS object, hence we can print using curly braces - {heading}</h3>
+    </div>);
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+//root.render(heading);
+//root.render(jsxHeading);
+root.render(<HeadingComponent />);
